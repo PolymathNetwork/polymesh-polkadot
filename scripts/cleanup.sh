168245a2be064434f792ce72292fb702a466981d
Jeremias Diaz <monitz87@gmail.com>
2020-02-06 02:03:32 -0300

    build: fix cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+if test -d packages-tmp; then
+  echo "restoring packages dir..."
+  rm -rf ./packages
+  mv ./packages-tmp ./packages
+fi
 
-rm -rf ./packages
-mv ./packages-tmp ./packages
-rm -rf ./tsconfig.json
-mv ./tsconfig-tmp.json ./tsconfig.json
+if test -f tsconfig-tmp.json then
+  echo "restoring tsconfig..."
+  rm -rf ./tsconfig.json
+  mv ./tsconfig-tmp.json ./tsconfig.json
+fi
