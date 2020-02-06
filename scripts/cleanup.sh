04f1e8e4d9c5bc538d4e5916ae05cd444c3d5919
Jeremias Diaz <monitz87@gmail.com>
2020-02-06 02:09:12 -0300

    chore: remove echo statements

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 if test -d packages-tmp; then
-  echo "restoring packages dir..."
   rm -rf ./packages
   mv ./packages-tmp ./packages
 fi
 
-if test -f tsconfig-tmp.json then
-  echo "restoring tsconfig..."
+if test -f tsconfig-tmp.json; then
   rm -rf ./tsconfig.json
   mv ./tsconfig-tmp.json ./tsconfig.json
 fi
